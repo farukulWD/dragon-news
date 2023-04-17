01690099838190926a861fb55360f03e1524f71d
Farukul Islam <farukgb1999@gmail.com>
2023-04-18 00:19:58 +0600

    right navigaition content added

---
+++ src/pages/shared/Qzone/Qzone.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import qzone1 from "../../../assets/qZone1.png"
+import qzone2 from "../../../assets/qZone2.png"
+import qzone3 from "../../../assets/qZone3.png"
+
+const Qzone = () => {
+    return (
+        <div>
+            <h4>Q Zone</h4>
+            <div className='bg-light'>
+                <img className='my-2' src={qzone1} alt="" />
+                <img className='my-2' src={qzone2} alt="" />
+                <img className='my-2' src={qzone3} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Qzone;
--- src/pages/shared/RightNav/RightNav.jsx
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import Qzone from "../Qzone/Qzone";
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h4>Login with</h4>
+      <div>
+        <Button className="mb-2" variant="outline-primary">
+          {" "}
+          <FaGoogle /> Login with Google
+        </Button>{" "}
+        <Button variant="outline-secondary">
+          {" "}
+          <FaGithub /> Login with Github
+        </Button>{" "}
+      </div>
+      <div className="my-3">
+        <h4 className="">Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item> < FaFacebook/> Facebook</ListGroup.Item>
+          <ListGroup.Item>< FaTwitter/> Twitter</ListGroup.Item>
+          <ListGroup.Item> < FaInstagram /> Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <Qzone></Qzone>
+      </div>
+    </div>
+  );
 };
 
 export default RightNav;
