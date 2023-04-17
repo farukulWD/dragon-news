a308fd468686c5a37009678b5f8b81f8144b2fe2
Farukul Islam <farukgb1999@gmail.com>
2023-04-17 23:46:00 +0600

    new dragon projects start

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh9GyhnM8XFa_NSW_yYgAirgj-6ya1FtA",
+  authDomain: "dragon-news-f0f8a.firebaseapp.com",
+  projectId: "dragon-news-f0f8a",
+  storageBucket: "dragon-news-f0f8a.appspot.com",
+  messagingSenderId: "261162863289",
+  appId: "1:261162863289:web:07b516b22931701ea62968"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'><small>© faruk</small></p>
+        </div>
+    );
+};
+
+export default Footer;
