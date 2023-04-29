7e62c4d422111ef27fcf601770bbdc6aec23510d
Farukul Islam <farukgb1999@gmail.com>
2023-04-29 20:29:01 +0600

    env file create and api key setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 console.log(import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyDh9GyhnM8XFa_NSW_yYgAirgj-6ya1FtA",
-  authDomain: "dragon-news-f0f8a.firebaseapp.com",
-  projectId: "dragon-news-f0f8a",
-  storageBucket: "dragon-news-f0f8a.appspot.com",
-  messagingSenderId: "261162863289",
-  appId: "1:261162863289:web:07b516b22931701ea62968"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
